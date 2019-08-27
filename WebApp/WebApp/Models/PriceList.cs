62580c2b22a6e563df12d6421fe994c675d6f378
Stefan Stipanovic <s_stefan@outlook.com>
2019-08-27 20:00:47 +0200

    .gitignore

---
+++ WebApp/WebApp/Models/PriceList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class PriceList
+    {
+        public int Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+}
---
+++ WebApp/WebApp/Models/TicketType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class TicketType
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
